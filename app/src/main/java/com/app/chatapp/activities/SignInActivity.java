330304982e379d326cce7e40cd568956b2ea4cc9
Arijit-Kumar-Pandab <arijitkuamrpandab@gmail.com>
2022-11-15 10:38:02 +0530

    feat:add sign-up functionality and sign-in layout

---
+++ app/src/main/java/com/app/chatapp/activities/SignInActivity.java
@@ -0,0 +1,32 @@
+package com.app.chatapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.app.chatapp.R;
+import com.app.chatapp.databinding.ActivitySignInBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textCreateNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+
+    }
+
+}
