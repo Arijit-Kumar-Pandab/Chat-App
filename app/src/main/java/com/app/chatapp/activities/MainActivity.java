3a96a954f12f5a8228074289386f6e4b1a89b2fd
Arijit-Kumar-Pandab <arijitkuamrpandab@gmail.com>
2022-11-17 23:13:36 +0530

    feat:add user data load feature to logout layout

--- app/src/main/java/com/app/chatapp/activities/MainActivity.java
+++ app/src/main/java/com/app/chatapp/activities/MainActivity.java
@@ -1,15 +1,35 @@
 package com.app.chatapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 
 import com.app.chatapp.R;
+import com.app.chatapp.databinding.ActivityMainBinding;
+import com.app.chatapp.utilities.Constants;
+import com.app.chatapp.utilities.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        loadUserDetails();
+    }
+
+    private void loadUserDetails() {
+        binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        binding.imageProfile.setImageBitmap(bitmap);
     }
 }
